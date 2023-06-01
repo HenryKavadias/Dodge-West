0fef88d682cf8c52d4512642baf7ec0fafa9d96e
Henry Kavadias-Barnes <bkyrneh@gmail.com>
2023-06-01 18:47:47 +1000

    Removed redundant scripts

--- Dodge West/Assets/Scripts/MaterialContainer.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "MaterialContainer", menuName = "ScriptableObjects/MaterialContainer")]
-public class MaterialContainer : ScriptableObject
-{
-    public Material[] materials;
-}
