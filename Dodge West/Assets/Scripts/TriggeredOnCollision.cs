109e1dc9062a3d9500dc8341176512a638175a76
Henry Kavadias-Barnes <bkyrneh@gmail.com>
2023-09-14 15:22:56 +1000

    Created an On collision trigger script, which triggeres an array of assigned functions when an object collides with another

---
+++ Dodge West/Assets/Scripts/TriggeredOnCollision.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class TriggeredOnCollision : MonoBehaviour
+{
+    public float minVelocity = 1.0f;
+    [Range(0.0f, 1.0f)] public float velocityThreshold = 0.1f;
+    public bool enableLimit = true;
+
+    public UnityEvent onCollision;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (enableLimit)
+        {
+            float factor = gameObject.GetComponent<Rigidbody>().velocity.magnitude / minVelocity;
+
+            if (factor > velocityThreshold)
+            {
+                onCollision?.Invoke();
+            }
+        }
+        else
+        {
+            onCollision?.Invoke();
+        }
+    }
+}
